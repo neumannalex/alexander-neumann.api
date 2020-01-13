5c7e6c846e64b2cca3388cf2d7add69de0a8e939
neumannalex <alexander.an.neumann@googlemail.com>
2020-01-13 18:26:56 +0100

    Projektdateien hinzufügen.

---
+++ Controllers/ClaimsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace alexander_neumann.api.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClaimsController : ControllerBase
+    {
+        [HttpGet()]
+        public ActionResult GetUserClaims()
+        {
+            try
+            {
+                if (HttpContext.User == null)
+                    return Unauthorized("User is null.");
+
+                if (HttpContext.User.Identity == null)
+                    return Unauthorized("Identity is null.");
+
+                if (!HttpContext.User.Identity.IsAuthenticated)
+                    return Unauthorized("User is not authenticated");
+
+                var claims = HttpContext.User.Claims.Select(x => new { x.Type, x.Value }).ToList();
+
+                return Ok(claims);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.AzureADB2C.UI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NSwag;
+using NSwag.Generation.Processors.Security;
+using NSwag.AspNetCore;
+
+namespace alexander_neumann.api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+
+            services.AddAuthentication(AzureADB2CDefaults.BearerAuthenticationScheme)
+                .AddAzureADB2CBearer(options => Configuration.Bind("AzureAdB2C", options));
+
+            services.AddOpenApiDocument(document =>
+            {
+                document.AddSecurity("bearer", Enumerable.Empty<string>(), new OpenApiSecurityScheme
+                {
+                    Type = OpenApiSecuritySchemeType.OAuth2,
+                    Description = "B2C authentication",
+                    Flow = OpenApiOAuth2Flow.Implicit,
+                    Flows = new OpenApiOAuthFlows()
+                    {
+                        Implicit = new OpenApiOAuthFlow()
+                        {
+                            Scopes = new Dictionary<string, string>
+                            {
+                                { "https://alexanderneumann.onmicrosoft.com/api/user_impersonation", "Access the api as the signed-in user" },
+                                { "https://alexanderneumann.onmicrosoft.com/api/api.read", "Read access to the API"},
+                                { "https://alexanderneumann.onmicrosoft.com/api/api.write", "Write access to the API"}
+                            },
+                            AuthorizationUrl = "https://alexanderneumann.b2clogin.com/alexanderneumann.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_signin1",
+                            TokenUrl = "https://alexanderneumann.b2clogin.com/alexanderneumann.onmicrosoft.com/oauth2/v2.0/token?p=B2C_1_signin1"
+                        },
+                    }
+                }); ;
+
+                document.OperationProcessors.Add(new AspNetCoreOperationSecurityScopeProcessor("bearer"));
+            });
+
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseOpenApi();
+            app.UseSwaggerUi3(settings =>
+            {
+                settings.OAuth2Client = new OAuth2ClientSettings
+                {
+                    ClientId = "570c8380-4a04-4e91-93fe-8b0eb356230d",
+                    AppName = "swagger-ui-client"
+                };
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
